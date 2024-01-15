173a0e4002c7535167c8c998aad88f870b161712
Deepak2200 <119037072+Deepak2200@users.noreply.github.com>
2024-01-15 20:19:43 +0530

    logger_file

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from datetime import datetime
+
+
+# here we make our path in our current working directory
+log_file=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_path=os.path.join(os.getcwd(),"logs",log_file)
+os.makedirs(log_path,exist_ok=True)
+
+# here we make final full path of our log file(complete path)
+LOG_FILE_PATH=os.path.join(log_path,log_file)
+
+
+# log file formate by basicConfig
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    formate="[ %(asctime)s ] %(lineno)d %(name)s - %(lavelname)s - %(message)s",
+    lavel=logging.INFO,
+)
